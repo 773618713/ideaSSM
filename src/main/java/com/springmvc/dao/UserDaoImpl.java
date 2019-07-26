9f037f1b11a1aa595d84a73e2ebf805a5a8e516e
773618713 <sunchaoyang123>
2019-07-26 11:35:23 +0800

    IDEA整合github测试

--- src/main/java/com/springmvc/dao/UserDaoImpl.java
+++ src/main/java/com/springmvc/dao/UserDaoImpl.java
@@ -1,21 +1,25 @@
 package com.springmvc.dao;
 
+import com.springmvc.controller.UserConroller;
 import com.springmvc.entity.ScoreUsers;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Repository
 public class UserDaoImpl {
 
     @Autowired
     private ScoreUsersMapper mapper;
+    Logger logger = Logger.getLogger(UserDaoImpl.class);
 
     public void insert() {
         ScoreUsers scoreUser = new ScoreUsers();
-        scoreUser.setUsername("test");
+        scoreUser.setUsername(UUID.randomUUID().toString());
         scoreUser.setUsercaption("1");
         scoreUser.setServerVer("1");
         scoreUser.setUsertype("1");
@@ -28,7 +32,18 @@ public class UserDaoImpl {
         scoreUser.setFilepath("1");
 
         int result = mapper.insert(scoreUser);
-        System.out.println(result);
+        logger.info(scoreUser);
+        logger.info("123456");
         assert (result == 1);
     }
+
+    /**
+     * 查询
+     * @param username
+     * @return
+     */
+    public ScoreUsers selectByPrimaryKey(String username){
+        return mapper.selectByPrimaryKey(username);
+    }
+
 }
